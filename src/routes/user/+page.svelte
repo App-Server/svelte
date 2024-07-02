aed8a47b808db4ed331060b88bdf64b809eebc23
Aragao <epicappcode@gmail.com>
2024-07-01 22:06:16 -0300

    first commit

---
+++ src/routes/user/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Link</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cumque ab maiores enim ratione mollitia veniam voluptate optio, modi facere officiis, iusto nesciunt quis eligendi laboriosam numquam quidem debitis assumenda?</p>
